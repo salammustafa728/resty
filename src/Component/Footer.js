8f2f43f1ed6a31c1064d4cf3706605bc2cd19465
salammustafa728 <salammustafa728@gmail.com>
2022-04-25 21:57:24 +0300

    solve lab 26

---
+++ src/Component/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import '../main.css';
+const Footer = () => {
+  return (
+    <div className="footer-div" style={{}}>
+      <div className="text-center">
+        <h3>Authored</h3>
+        <div>
+          <h3>Salam Mustafa</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
